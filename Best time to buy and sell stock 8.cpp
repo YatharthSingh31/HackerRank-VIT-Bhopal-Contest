a0e6d2d8f61e1f822e8e1af3c673e0c548116f36
Yatharth Singh <80636128+YatharthSingh31@users.noreply.github.com>
2023-04-16 13:52:37 +0530

    Create Best time to buy and sell stock 8.cpp

---
+++ Best time to buy and sell stock 8.cpp
@@ -0,0 +1,70 @@
+/*You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Input Format
+
+First line of each test case or query contains an integer 'N' representing the size of the first array/list.
+Second line contains 'N' single space separated integers representing the elements in the array/list.
+Constraints
+
+No
+
+Output Format
+
+Integer representing the answer of the problem statement.
+
+Sample Input 0
+
+6
+7 1 5 3 6 4
+Sample Output 0
+
+5
+Explanation 0
+
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Sample Input 1
+
+5
+7 6 4 3 1
+Sample Output 1
+
+0
+*/
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.size(); i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+        
+    }
+
+int main() {
+    int n;
+    cin>>n;
+    vector <int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    int result = maxProfit(v);
+    cout<<result;
+    return 0;
+}
